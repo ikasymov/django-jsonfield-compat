7475ef8fdd9253351ccc95a3837c3781bd3269cd
Ilgiz kasymov <ikasymov@else.local>
2020-01-07 13:09:29 +0600

    avoid crash on migrate

--- jsonfield_compat/convert.py
+++ jsonfield_compat/convert.py
@@ -20,19 +20,21 @@ def convert_column_to_json(model, column_name):
             "where table_name = %s and column_name = %s;",
             [table_name, column_name])
 
-        current_type = cursor.fetchone()[0].upper()
-        expected_type = 'JSONB' if use_native_jsonfield() else 'TEXT'
+        row = cursor.fetchone()
+        if row is not None:
+            current_type = row[0].upper()
+            expected_type = 'JSONB' if use_native_jsonfield() else 'TEXT'
 
-        if current_type != expected_type:
-            print("{app}: Converting {col} to use native {type} field".format(
-                app=model._meta.app_label, col=column_name, type=expected_type))
+            if current_type != expected_type:
+                print("{app}: Converting {col} to use native {type} field".format(
+                    app=model._meta.app_label, col=column_name, type=expected_type))
 
-            cursor.execute(
-                "ALTER TABLE {table} ALTER COLUMN {col} "
-                "TYPE {type} USING {col}::{type};".format(
-                    table=table_name, col=column_name, type=expected_type
+                cursor.execute(
+                    "ALTER TABLE {table} ALTER COLUMN {col} "
+                    "TYPE {type} USING {col}::{type};".format(
+                        table=table_name, col=column_name, type=expected_type
+                    )
                 )
-            )
 
 
 def convert_model_json_fields(model):
